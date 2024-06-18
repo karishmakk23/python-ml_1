9d60a136683724818be5423666cfc13a2b2ec463
KARISHMA KUMARI <104776872+karishmakk23@users.noreply.github.com>
2024-06-18 20:50:44 +0530

    Add files via upload

---
+++ File_1.py
@@ -0,0 +1,11 @@
+# Write a program to take two numbers as input and print their sum:
+
+num1= int(input("Enter the first number:"))    # Taking input from the user
+num2= int(input("Enter the second number:"))
+
+sum= num1+num2
+print(sum)
+
+
+
+
