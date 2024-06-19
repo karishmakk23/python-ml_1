fca9f71de072568cec3da0ac995d78351e0c69af
KARISHMA KUMARI <104776872+karishmakk23@users.noreply.github.com>
2024-06-20 02:42:33 +0530

    Add files via upload

---
+++ file_21.py
@@ -0,0 +1,8 @@
+# write a python program that converts counts the occurences of a specific element in a list:
+
+list=[2,4,6,7,9,1,4,6,54,7]
+element=int(input("Enter the element:"))
+
+occurences= list.count(element)
+print(occurences)
+
